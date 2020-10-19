4184ab6e26b9f7b3a576bc7ce1db7386b5e4a1d8
Shota Fuji <pockawoooh@gmail.com>
2020-10-19 14:52:57 +0000

    feat(react): React bindings
    You can use WebComponents (custom elements) in React, but the experience
    is not great. No props binding (React uses setAttribute), Poor type
    definition (JSX does great job though), Imperative event handling,
    etc...
    
    This bindings aim to solve these inconveniencies.

---
+++ packages/react/src/index.stories.jsx
@@ -0,0 +1,26 @@
+import { FigspecViewer } from ".";
+
+import demoJson from "./__storybook__/demo-data/Klm6pxIZSaJFiOMX5FpTul9F/64:1.json";
+import demoImage from "./__storybook__/demo-data/Klm6pxIZSaJFiOMX5FpTul9F/64:1.svg";
+
+export default {
+  title: "FigspecViewer",
+  component: FigspecViewer,
+};
+
+const Template = (args) => (
+  <FigspecViewer
+    {...args}
+    style={{
+      width: 500,
+      height: 500,
+    }}
+  />
+);
+
+export const demo = Template.bind({});
+
+demo.args = {
+  nodes: demoJson,
+  renderedImage: demoImage,
+};
