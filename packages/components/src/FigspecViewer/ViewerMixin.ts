9547cdf22e92b6b30f9e0162027e5d9714fcacd0
Shota Fuji <pockawoooh@gmail.com>
2021-05-26 20:02:26 +0000

    fix(components): Use Figma's default background colour
    https://github.com/pocka/figspec/issues/24

--- packages/components/src/FigspecViewer/ViewerMixin.ts
+++ packages/components/src/FigspecViewer/ViewerMixin.ts
@@ -75,7 +75,7 @@ export const ViewerMixin = <T extends Constructor<LitElement>>(
             --default-error-bg: #fff;
             --default-error-fg: #333;
 
-            --bg: var(--figspec-viewer-bg, #666);
+            --bg: var(--figspec-viewer-bg, #E5E5E5);
             --z-index: var(--figspec-viewer-z-index, 0);
             --error-bg: var(--figspec-viewer-error-bg, var(--default-error-bg));
             --error-fg: var(--figspec-viewer-error-fg, var(--default-error-fg));
